a81fb791b9df0d3ee3185b7bc09ff5c5ccd7c943
lpchg1992 <github@imlpc.com>
2020-12-23 20:17:22 +0800

    mock

--- ant-design-vue-pro/src/views/Dashboard/Analysis.vue
+++ ant-design-vue-pro/src/views/Dashboard/Analysis.vue
@@ -6,41 +6,54 @@
 
 <script>
 import Chart from "../../components/Chart";
-import random from "lodash/random";
+// import random from "lodash/random";
+import axios from "axios";
 export default {
   components: {
     Chart,
   },
   data() {
     return {
-      chartOption: {
-        title: {
-          text: "ECharts 入门示例",
-        },
-        tooltip: {},
-        xAxis: {
-          data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
-        },
-        yAxis: {},
-        series: [
-          {
-            name: "销量",
-            type: "bar",
-            data: [5, 20, 36, 10, 10, 20],
-          },
-        ],
-      },
+      chartOption: {},
     };
   },
   // 每3秒触发一次数据更改。
   mounted() {
+    this.getChartData();
     this.interval = setInterval(() => {
-      this.chartOption.series[0].data = this.chartOption.series[0].data.map(
-        () => random(100)
-      );
+      this.getChartData();
+      // this.chartOption.series[0].data = this.chartOption.series[0].data.map(
+      //   () => random(100)
+      // );
       // 当没有深度监听时调用
       this.chartOption = { ...this.chartOption };
     }, 3000);
+  },
+  methods: {
+    getChartData() {
+      // get的同时传递参数，当参数返回调用then
+      axios
+        .get("/api/dashboard/chart", { params: { ID: 12345 } })
+        .then(response => {
+          this.chartOption = {
+            title: {
+              text: "ECharts 入门示例",
+            },
+            tooltip: {},
+            xAxis: {
+              data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+            },
+            yAxis: {},
+            series: [
+              {
+                name: "销量",
+                type: "bar",
+                data: response.data
+              },
+            ],
+          };
+        });
+    },
   },
   beforeDestroy() {
     clearInterval(this.interval);
--- ant-design-vue-pro/vue.config.js
+++ ant-design-vue-pro/vue.config.js
@@ -8,5 +8,30 @@ module.exports = {
         javascriptEnabled: true
       }
     }
+  },
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        bypass: function(req, res) {
+          if (req.headers.accept.indexOf("html") !== -1) {
+            console.log("Skipping proxy for browser request.");
+            return "/index.html";
+          } else {
+            // 将非html请求截获，对路由字符串进行处理，返回对应js文件的名称。
+            const name = req.path
+              .split("/api/")[1]
+              .split("/")
+              .join("_");
+            // require后，数据变化不生效。
+            const mock = require(`./mock/${name}`);
+            const result = mock(req.method);
+            // 所以需要清理缓存。
+            delete require.cache[require.resolve(`./mock/${name}`)];
+            return res.send(result);
+          }
+        }
+      }
+    }
   }
-}
+};
